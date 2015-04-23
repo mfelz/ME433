d2b7de9139164ef6e7970266243bc2d263f6eeb1
Matt Felz <mattfelz@gmail.com>
2015-04-22 21:28:40 -0500

    Custom Mouse Project

---
+++ hw7/src/main.c
@@ -0,0 +1,237 @@
+/*******************************************************************************
+  MPLAB Harmony Project Main Source File
+
+  Company:
+    Microchip Technology Inc.
+  
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for an MPLAB Harmony project.
+
+  Description:
+    This file contains the "main" function for an MPLAB Harmony project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state 
+    machines of all MPLAB Harmony modules in the system and it calls the 
+    "SYS_Tasks" function from within a system-wide "super" loop to maintain 
+    their correct operation. These two functions are implemented in 
+    configuration-specific files (usually "system_init.c" and "system_tasks.c")
+    in a configuration-specific folder under the "src/system_config" folder 
+    within this project's top-level folder.  An MPLAB Harmony project may have
+    more than one configuration, each contained within it's own folder under
+    the "system_config" folder.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
+//Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "system/common/sys_module.h"   // SYS function prototypes
+#include <xc.h>
+#include "accel.h"
+#include "i2c_display.h"
+#include "i2c_master_int.h"
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+static const char ASCII[96][5] = {
+    {0x00, 0x00, 0x00, 0x00, 0x00} // 20  (space)
+    ,{0x00, 0x00, 0x5f, 0x00, 0x00} // 21 !
+    ,{0x00, 0x07, 0x00, 0x07, 0x00} // 22 "
+    ,{0x14, 0x7f, 0x14, 0x7f, 0x14} // 23 #
+    ,{0x24, 0x2a, 0x7f, 0x2a, 0x12} // 24 $
+    ,{0x23, 0x13, 0x08, 0x64, 0x62} // 25 %
+    ,{0x36, 0x49, 0x55, 0x22, 0x50} // 26 &
+    ,{0x00, 0x05, 0x03, 0x00, 0x00} // 27 '
+    ,{0x00, 0x1c, 0x22, 0x41, 0x00} // 28 (
+    ,{0x00, 0x41, 0x22, 0x1c, 0x00} // 29 )
+    ,{0x14, 0x08, 0x3e, 0x08, 0x14} // 2a *
+    ,{0x08, 0x08, 0x3e, 0x08, 0x08} // 2b +
+    ,{0x00, 0x50, 0x30, 0x00, 0x00} // 2c ,
+    ,{0x08, 0x08, 0x08, 0x08, 0x08} // 2d -
+    ,{0x00, 0x60, 0x60, 0x00, 0x00} // 2e .
+    ,{0x20, 0x10, 0x08, 0x04, 0x02} // 2f /
+    ,{0x3e, 0x51, 0x49, 0x45, 0x3e} // 30 0
+    ,{0x00, 0x42, 0x7f, 0x40, 0x00} // 31 1
+    ,{0x42, 0x61, 0x51, 0x49, 0x46} // 32 2
+    ,{0x21, 0x41, 0x45, 0x4b, 0x31} // 33 3
+    ,{0x18, 0x14, 0x12, 0x7f, 0x10} // 34 4
+    ,{0x27, 0x45, 0x45, 0x45, 0x39} // 35 5
+    ,{0x3c, 0x4a, 0x49, 0x49, 0x30} // 36 6
+    ,{0x01, 0x71, 0x09, 0x05, 0x03} // 37 7
+    ,{0x36, 0x49, 0x49, 0x49, 0x36} // 38 8
+    ,{0x06, 0x49, 0x49, 0x29, 0x1e} // 39 9
+    ,{0x00, 0x36, 0x36, 0x00, 0x00} // 3a :
+    ,{0x00, 0x56, 0x36, 0x00, 0x00} // 3b ;
+    ,{0x08, 0x14, 0x22, 0x41, 0x00} // 3c <
+    ,{0x14, 0x14, 0x14, 0x14, 0x14} // 3d =
+    ,{0x00, 0x41, 0x22, 0x14, 0x08} // 3e >
+    ,{0x02, 0x01, 0x51, 0x09, 0x06} // 3f ?
+    ,{0x32, 0x49, 0x79, 0x41, 0x3e} // 40 @
+    ,{0x7e, 0x11, 0x11, 0x11, 0x7e} // 41 A
+    ,{0x7f, 0x49, 0x49, 0x49, 0x36} // 42 B
+    ,{0x3e, 0x41, 0x41, 0x41, 0x22} // 43 C
+    ,{0x7f, 0x41, 0x41, 0x22, 0x1c} // 44 D
+    ,{0x7f, 0x49, 0x49, 0x49, 0x41} // 45 E
+    ,{0x7f, 0x09, 0x09, 0x09, 0x01} // 46 F
+    ,{0x3e, 0x41, 0x49, 0x49, 0x7a} // 47 G
+    ,{0x7f, 0x08, 0x08, 0x08, 0x7f} // 48 H
+    ,{0x00, 0x41, 0x7f, 0x41, 0x00} // 49 I
+    ,{0x20, 0x40, 0x41, 0x3f, 0x01} // 4a J
+    ,{0x7f, 0x08, 0x14, 0x22, 0x41} // 4b K
+    ,{0x7f, 0x40, 0x40, 0x40, 0x40} // 4c L
+    ,{0x7f, 0x02, 0x0c, 0x02, 0x7f} // 4d M
+    ,{0x7f, 0x04, 0x08, 0x10, 0x7f} // 4e N
+    ,{0x3e, 0x41, 0x41, 0x41, 0x3e} // 4f O
+    ,{0x7f, 0x09, 0x09, 0x09, 0x06} // 50 P
+    ,{0x3e, 0x41, 0x51, 0x21, 0x5e} // 51 Q
+    ,{0x7f, 0x09, 0x19, 0x29, 0x46} // 52 R
+    ,{0x46, 0x49, 0x49, 0x49, 0x31} // 53 S
+    ,{0x01, 0x01, 0x7f, 0x01, 0x01} // 54 T
+    ,{0x3f, 0x40, 0x40, 0x40, 0x3f} // 55 U
+    ,{0x1f, 0x20, 0x40, 0x20, 0x1f} // 56 V
+    ,{0x3f, 0x40, 0x38, 0x40, 0x3f} // 57 W
+    ,{0x63, 0x14, 0x08, 0x14, 0x63} // 58 X
+    ,{0x07, 0x08, 0x70, 0x08, 0x07} // 59 Y
+    ,{0x61, 0x51, 0x49, 0x45, 0x43} // 5a Z
+    ,{0x00, 0x7f, 0x41, 0x41, 0x00} // 5b [
+    ,{0x02, 0x04, 0x08, 0x10, 0x20} // 5c ¥
+    ,{0x00, 0x41, 0x41, 0x7f, 0x00} // 5d ]
+    ,{0x04, 0x02, 0x01, 0x02, 0x04} // 5e ^
+    ,{0x40, 0x40, 0x40, 0x40, 0x40} // 5f _
+    ,{0x00, 0x01, 0x02, 0x04, 0x00} // 60 `
+    ,{0x20, 0x54, 0x54, 0x54, 0x78} // 61 a
+    ,{0x7f, 0x48, 0x44, 0x44, 0x38} // 62 b
+    ,{0x38, 0x44, 0x44, 0x44, 0x20} // 63 c
+    ,{0x38, 0x44, 0x44, 0x48, 0x7f} // 64 d
+    ,{0x38, 0x54, 0x54, 0x54, 0x18} // 65 e
+    ,{0x08, 0x7e, 0x09, 0x01, 0x02} // 66 f
+    ,{0x0c, 0x52, 0x52, 0x52, 0x3e} // 67 g
+    ,{0x7f, 0x08, 0x04, 0x04, 0x78} // 68 h
+    ,{0x00, 0x44, 0x7d, 0x40, 0x00} // 69 i
+    ,{0x20, 0x40, 0x44, 0x3d, 0x00} // 6a j
+    ,{0x7f, 0x10, 0x28, 0x44, 0x00} // 6b k
+    ,{0x00, 0x41, 0x7f, 0x40, 0x00} // 6c l
+    ,{0x7c, 0x04, 0x18, 0x04, 0x78} // 6d m
+    ,{0x7c, 0x08, 0x04, 0x04, 0x78} // 6e n
+    ,{0x38, 0x44, 0x44, 0x44, 0x38} // 6f o
+    ,{0x7c, 0x14, 0x14, 0x14, 0x08} // 70 p
+    ,{0x08, 0x14, 0x14, 0x18, 0x7c} // 71 q
+    ,{0x7c, 0x08, 0x04, 0x04, 0x08} // 72 r
+    ,{0x48, 0x54, 0x54, 0x54, 0x20} // 73 s
+    ,{0x04, 0x3f, 0x44, 0x40, 0x20} // 74 t
+    ,{0x3c, 0x40, 0x40, 0x20, 0x7c} // 75 u
+    ,{0x1c, 0x20, 0x40, 0x20, 0x1c} // 76 v
+    ,{0x3c, 0x40, 0x30, 0x40, 0x3c} // 77 w
+    ,{0x44, 0x28, 0x10, 0x28, 0x44} // 78 x
+    ,{0x0c, 0x50, 0x50, 0x50, 0x3c} // 79 y
+    ,{0x44, 0x64, 0x54, 0x4c, 0x44} // 7a z
+    ,{0x00, 0x08, 0x36, 0x41, 0x00} // 7b {
+    ,{0x00, 0x00, 0x7f, 0x00, 0x00} // 7c |
+    ,{0x00, 0x41, 0x36, 0x08, 0x00} // 7d }
+    ,{0x10, 0x08, 0x08, 0x10, 0x08} // 7e ?
+    ,{0x00, 0x06, 0x09, 0x09, 0x06} // 7f ?
+}; // end char ASCII[96][5]
+
+void dispChar(char msg[], int row, int col);
+int getBit (int ltr, int jj, int kk);
+void screenReset();
+
+int main ( void )
+{
+    screenReset();
+    /* Initialize all MPLAB Harmony modules, including application(s). */
+    SYS_Initialize ( NULL );
+
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
+void dispChar(char msg[], int row, int col){
+    int len = strlen(msg);
+    int ltr;
+
+    int ii=0;
+    int jj=0;
+    int kk=0;
+    int numlet = 0;
+    int crow = 0;
+
+    for (ii=0; ii<len; ii++) { //for each letter in string
+        ltr = (int) msg[ii] - 0x20; //set character ref
+        for (jj=0; jj<5; jj++){ //cycle through columns of ascii table
+            for (kk=0; kk<8; kk++) { //cycle through rows
+                int bit = getBit(ltr,jj,kk);
+                if(bit){
+                    display_pixel_set(row-kk-crow*8,col-jj-5*numlet,1);
+                }
+                else {
+                    display_pixel_set(row-kk-crow*8,col-jj-5*numlet,0);
+                }
+            }
+        }
+        numlet++;
+        if (-numlet*5+col< 5 || msg[ii] == "\n") {
+            numlet = 0;
+            crow++;
+        }
+    }
+    display_draw();
+}
+
+int getBit(int ltr, int jj, int kk) {
+    return (ASCII[ltr][jj] & (1 << (kk-1))) >> (kk-1);
+}
+
+void screenReset() {
+    LATBbits.LATB2 = 0;
+    LATBbits.LATB2 = 1;
+}
